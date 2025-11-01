a9a0c9e71ab2ef41e4c57f968eb33c685ed7bb9e
denliedesign <customdenlie@gmail.com>
2025-11-01 12:01:08 -0500

    fix meta titles

--- resources/views/components/layouts/app.blade.php
+++ resources/views/components/layouts/app.blade.php
@@ -1,8 +1,20 @@
-<x-layouts.app.header :title="$title ?? null">
+@php
+    $pageTitle = $title ?? 'Isabella Lown';
+    $displayTitle = str($pageTitle)->contains('Isabella Lown |')
+        ? str($pageTitle)->after('Isabella Lown | ')->trim()
+        : $pageTitle;
+@endphp
+
+<x-layouts.app.header :title="$pageTitle">
     <flux:main container>
-        <div class="lg:hidden flex justify-center" style="width: 100% !important;"><x-app-logo /></div>
-        <h1 class="text-center lg:hidden font-semibold underline uppercase mb-3" style="font-size: 25px;">{{ str($title)->after('Isabella Lown | ')->trim() }}</h1>
+        <div class="lg:hidden flex justify-center" style="width: 100% !important;">
+            <x-app-logo />
+        </div>
+        <h1 class="text-center lg:hidden font-semibold underline uppercase mb-3" style="font-size: 25px;">
+            {{ $displayTitle }}
+        </h1>
         {{ $slot }}
         @include('components._footer')
     </flux:main>
 </x-layouts.app.header>
+
